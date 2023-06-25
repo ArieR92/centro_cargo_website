8320fc5d538611ce2db9a8e09f41e21313319fc3
Ariel Rodriguez <arielrodriguez0@outlook.com>
2023-06-25 23:24:27 +0200

    services-changed

--- js/javascript.js
+++ js/javascript.js
@@ -12,6 +12,29 @@ $('.readmore-btn').on('click', function(){
     // El código aquí se ejecutará cuando el DOM esté listo
     console.log("jQuery se ha cargado correctamente.");
   });
+
+
+ //show less/show more card with the box sizes
+
+ document.addEventListener("DOMContentLoaded", function() {
+
+  function toggleText2() {
+    const hiddenText2 = document.querySelector('.hidden-text-2');
+    hiddenText2.classList.toggle('d-flex');
+  }
+  
+  const button2 = document.querySelector('.btn');
+  button2.addEventListener('click', toggleText2);
+});
+
+
+ 
+
+
+
+
+
+
   
 
 
